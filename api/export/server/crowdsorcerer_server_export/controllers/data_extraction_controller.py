01906d3dc8d56e7c9e2c513cee7b0bde71381630
Martinho Tavares <martinho.tavares@ua.pt>
2022-06-04 10:20:27 +0100

    Redesigned endpoints to allow multiple formats, restored README

--- api/export/server/crowdsorcerer_server_export/controllers/data_extraction_controller.py
+++ api/export/server/crowdsorcerer_server_export/controllers/data_extraction_controller.py
@@ -1,16 +1,30 @@
 import connexion
 import six
 
-from .implementation import extract
+from crowdsorcerer_server_export import util
+from crowdsorcerer_server_export.controllers.implementation import extract
+from crowdsorcerer_server_export.df2CKAN import EXPORT_FORMATS
 
 
-def data_extraction(format_, date_from=None, date_to=None, types=None, units=None):  # noqa: E501
+def available_formats():  # noqa: E501
+    """List of the available extraction formats
+
+     # noqa: E501
+
+
+    :rtype: str
+    """
+    
+    return str(list(EXPORT_FORMATS.keys()))[1:-1].replace("'", "")
+
+
+def data_extraction(formats, date_from=None, date_to=None, types=None, units=None):  # noqa: E501
     """Extract data from the data lake into a CKAN compliant format, zipped.
 
      # noqa: E501
 
-    :param format: The case insensitive string representing the dataset&#x27;s output format
-    :type format: str
+    :param formats: The case insensitive strings representing the dataset&#x27;s output formats
+    :type formats: List[str]
     :param date_from: Only data from this date forwards will be extracted (UTC+0, in ISO 8601 format), inclusive
     :type date_from: str
     :param date_to: Only data from this date backwards will be extracted (UTC+0, in ISO 8601 format), inclusive
@@ -23,4 +37,4 @@ def data_extraction(format_, date_from=None, date_to=None, types=None, units=Non
     :rtype: None
     """
 
-    return extract(format_, date_from, date_to, types, units)
+    return extract(formats, date_from, date_to, types, units)
--- api/export/server/crowdsorcerer_server_export/controllers/implementation.py
+++ api/export/server/crowdsorcerer_server_export/controllers/implementation.py
@@ -1,6 +1,7 @@
 import io
 import json
 import zipfile
+from typing import List
 from dateutil.parser import ParserError
 from datetime import date, datetime, timedelta, timezone
 
@@ -20,12 +21,13 @@ BASE_METADATA = {
     'notes': 'Crowdsourced smart home data collected from the CrowdSorcerer open source project. More info on https://smarthouse.av.it.pt'
 }
 
-def extract(format_, date_from=None, date_to=None, types=None, units=None):
+def extract(formats: List[str], date_from=None, date_to=None, types=None, units=None):
 
-    format_ = format_.lower()
-    if format_ not in EXPORT_FORMATS:
-        raise UnsupportedExportationFormat(format_)
-    convert = EXPORT_FORMATS[format_]
+    formats = [ f.lower() for f in formats ]
+    for f in formats:
+        if f not in EXPORT_FORMATS:
+            raise UnsupportedExportationFormat(f)
+    converters = [ EXPORT_FORMATS[f] for f in formats ]
 
     try:
         date_from = util.deserialize_date(date_from) if date_from else None
@@ -44,26 +46,28 @@ def extract(format_, date_from=None, date_to=None, types=None, units=None):
         units=units
     )
 
-    dataset_data = convert(df).encode(encoding='utf-8')
-    
     now = datetime.now(tz=timezone(timedelta(hours=0)))
     yesterday = date.today() - timedelta(days=1)
+    resources = [{
+        'package-id': BASE_METADATA['name'],
+        'url': 'upload-' + format_,
+        'format': format_,
+        'created': str(now),
+        'last_modified': str(yesterday)
+    } for format_ in formats]
     dataset_metadata = json.dumps({
         **BASE_METADATA,
-        'resources': [{
-            'package-id': BASE_METADATA['name'],
-            'url': 'upload',
-            'format': format_,
-            'created': str(now),
-            'last_modified': str(yesterday)
-        }],
+        'resources': resources,
         'extras': [extraction_details]
     }).encode(encoding='utf-8')
 
     zipped_data = io.BytesIO()
+
     z = zipfile.ZipFile(zipped_data, mode='w', compression=zipfile.ZIP_DEFLATED, compresslevel=9)
-    z.writestr(f'crowdsorcerer_extract.{format_}', dataset_data)
-    z.writestr(f'crowdsorcerer_extract_metadata_{format_}.json', dataset_metadata)
+    for format_, converter in zip(formats, converters):
+        dataset_data = converter(df).encode(encoding='utf-8')
+        z.writestr(f'crowdsorcerer_extract_{now.date()}.{format_}', dataset_data)
+    z.writestr(f'crowdsorcerer_extract_{now.date()}_metadata.json', dataset_metadata)
     z.close()
 
     zipped_data.seek(0)
@@ -72,4 +76,4 @@ def extract(format_, date_from=None, date_to=None, types=None, units=None):
         path_or_file=zipped_data,
         mimetype='application/zip',
         as_attachment=True,
-        attachment_filename=f'crowdsorcerer_extract.{format_}.zip')
+        attachment_filename=f'crowdsorcerer_extract_{now.date()}.zip')
